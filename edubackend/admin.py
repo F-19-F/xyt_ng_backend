802d06880decdfd4da97325d1b471595336a78a2
ff19 <80933556+ff19@users.noreply.github.com>
2022-07-10 21:57:42 +0800

    最终

--- edubackend/admin.py
+++ edubackend/admin.py
@@ -41,17 +41,22 @@ class UserModelAdmin(UserAdmin):
 @admin.register(Course)
 class CourseModelAdmin(admin.ModelAdmin):
     # Course.credit
+    search_fields = ('create_teacher__name','name')
+    #   = ('create_teacher',)
     list_display = ('name', 'create_teacher', 'credit')
 
 
 @admin.register(EduClass)
 class EduClassModelAdmin(admin.ModelAdmin):
     # EduClass.
+    list_filter = ('xq','xn','course')
     list_display = ('course','xq','xn', 'whichday', 'begin_in_day', 'end_in_day','classroom')
 
 
 @admin.register(PeopleClass)
 class PeopleClassModelAdmin(admin.ModelAdmin):
+    search_fields = ('student__name',)
+    list_filter = ('educlass',)
     list_display = ('educlass', 'student')
 
 
@@ -59,26 +64,33 @@ class PeopleClassModelAdmin(admin.ModelAdmin):
 class WorkModelAdmin(admin.ModelAdmin):
     # Work.
     list_display = ('educlass', 'title')
+    list_filter = ('educlass',)
 
 
 @admin.register(Answer)
 class AnswerModelAdmin(admin.ModelAdmin):
     # Answer.
+    search_fields = ('people__name',)
     list_display = ('people', 'work', 'content')
 
 
 @admin.register(Score)
 class ScoreModelAdmin(admin.ModelAdmin):
     # Score.
+    list_filter = ('educlass',)
+
+    search_fields = ('people__name',)
     list_display = ('people', 'score', 'educlass')
 
 
 @admin.register(ClassRoom)
 class ClassRoomModelAdmin(admin.ModelAdmin):
+    list_filter = ('buildingname',)
     list_display = ("buildingname", "roomname")
 
 
 @admin.register(Exam)
 class ExamModelAdmin(admin.ModelAdmin):
+    list_filter = ('educlass','classroom')
     # Exam.classroom
     list_display = ("educlass", "name","classroom" ,"begin_time", "end_time")
